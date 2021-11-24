48fcff01f3654c18b05adc60beb7f89e2244fc4f
Paul Guerra <paulguerra@Pauls-Air.home>
2021-11-23 22:31:52 -0600

    task name and type alert submitted, and form reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,15 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+
+    }
+
+    formEl.reset();
+
       // package up data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -33,6 +42,6 @@ var createTaskEl = function(taskDataObj) {
 
     // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-}
+};
 
 formEl.addEventListener("submit", taskFormHandler);
